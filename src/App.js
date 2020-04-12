adade7642688257b97c4fc235c29e4ce6e1a6b81
fuchis <ivan.lopez3k@gmail.com>
2020-04-12 03:55:01 -0500

    Created Auth page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import firebase from './utils/Firebase';
 import "firebase/auth";
+import Auth from './pages/Auth';
 
 function App() {
 
-  firebase.auth().onAuthStateChanged(currentUser => console.log(currentUser ? "estamos loggeados":"no estamos loggeados"));
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  firebase.auth().onAuthStateChanged(currentUser => {
+    if(!currentUser){
+      setUser(null);
+    }else {
+      setUser(currentUser);
+    }
+    setIsLoading(false);
+  });
+
+  if(isLoading) {
+    return null;
+  }
 
   return (
-    <div className="App">
-
-     <h1>HOLa</h1>
-
-   
-    </div>
+    !user ? <Auth/> : <UserLogged />
   );
+}
+
+const UserLogged = () => {
+  const logout = () => {
+    firebase.auth().signOut();
+  }
+  return(
+    <div style={{
+       display: "flex", 
+       alignItems: "center", 
+       justifyContent:"center",
+       flexDirection: "column",
+       height: "100vh",
+    }}>
+
+      <h1>Usuario loggeado</h1>
+      <button onClick={logout}>Cerrar Sesión</button>
+
+    </div>
+  )
 }
 
 export default App;
