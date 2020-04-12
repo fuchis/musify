b8707dc60b3513ba753e97a4a1bb75af56181ea7
fuchis <ivan.lopez3k@gmail.com>
2020-04-11 21:02:14 -0500

    Added Firebase support

---
+++ src/utils/Firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAcaNRVe3uHU8_Ks2L40xgy64RIRSM07F8",
+    authDomain: "musicfy-12f6a.firebaseapp.com",
+    databaseURL: "https://musicfy-12f6a.firebaseio.com",
+    projectId: "musicfy-12f6a",
+    storageBucket: "musicfy-12f6a.appspot.com",
+    messagingSenderId: "44106959031",
+ 	appId: "1:44106959031:web:8b47067c636053b149e532"
+};
+
+export default firebase.initializeApp(firebaseConfig);
