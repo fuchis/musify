e877c40891c1ba5d4e4063fd17734d51c2968eb4
fuchis <ivan.lopez3k@gmail.com>
2020-04-13 00:50:38 -0500

    Created AuthOptions buttons and AuthOptions Styles

--- src/components/Auth/AuthOptions/AuthOptions.js
+++ src/components/Auth/AuthOptions/AuthOptions.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
+import { Button } from 'semantic-ui-react'
+import "./AuthOptions.scss";
 
-const AuthOptions = () => {
+const AuthOptions = ({setSelectedForm}) => {
   return (
-    <div>
-        <h1>AuthOptions...</h1>
+    <div className="auth-options">
+      <h2>Millones de canciones, gratis en Musicfy</h2>
+      <Button className="register" onClick={() => setSelectedForm("register")}>
+        Registrate gratis
+      </Button>
+      <Button className="login" onClick={() => setSelectedForm("login")}>
+        Iniciar Sesión
+      </Button>
+
     </div>
   );
 }
