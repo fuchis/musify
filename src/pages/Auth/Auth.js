9be484f67fb1c7bae522f4d886afd050b1c94132
fuchis <ivan.lopez3k@gmail.com>
2020-04-13 00:53:58 -0500

    Created function to handle forms

--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AuthOptions from "../../components/Auth/AuthOptions";
+import RegisterForm from "../../components/Auth/RegisterForm";
+import LoginForm from "../../components/Auth/LoginForm";
+import BackgroundAuth from "../../assets/img/jpg/background-auth.jpg";
+import LogoNameWhite from "../../assets/img/png/logo-name-white.png";
+import "./Auth.scss";
 
 const Auth = () => {
+	
+	const [selectedForm, setSelectedForm] = useState(null);
+
+	const handledForm = () => {
+		switch(selectedForm) {
+			case "login": 
+				return <LoginForm setSelectedForm={setSelectedForm}/>
+			case "register":
+				return <RegisterForm setSelectedForm={setSelectedForm}/>; 
+			default:
+				return <AuthOptions setSelectedForm={setSelectedForm}/>
+		}
+	}
+
   return (
-    <div>
-        <h2>Iniciar Sesión</h2>
-        <h2>Registrarse</h2>
-    </div>
+		<div className="auth" style={{ backgroundImage: `url(${BackgroundAuth})`}}>
+			 <div className="auth__dark"></div>
+			 <div className="auth__box">
+				 <div className="auth__box-logo">
+					 <img src={LogoNameWhite} alt="Musicfy"/>
+				 </div>
+				 { handledForm() }
+			 </div>
+		</div>
   );
 }
 
