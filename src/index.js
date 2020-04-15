1b50dcf43fc011010624b61abe291c1557f6cfac
fuchis <ivan.lopez3k@gmail.com>
2020-04-14 20:29:15 -0500

    Added react-toastify to app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css'
 import './index.scss';
+import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
