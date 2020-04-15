07d5ded76996fcac8c8c738cabff3a795c28eb8f
fuchis <ivan.lopez3k@gmail.com>
2020-04-15 06:52:00 -0500

    Created Login Form

--- src/components/Auth/LoginForm/LoginForm.js
+++ src/components/Auth/LoginForm/LoginForm.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Button, Icon, Form, Input } from "semantic-ui-react";
+import { toast } from "react-toastify";
+import validateEmail from "../../../utils/Validations";
+import firebase from "../../../utils/Firebase";
+import "firebase/auth";
+import "./LoginForm.scss";
 
-const LoginForm = () => {
-    return(
-        <div>
-            <h1>LoginForm</h1>
-        </div>
-    )
+const LoginForm = ({ setSelectedForm }) => {
+	
+	const [showPassword, setShowPassword] = useState(false);
+	
+	const handleForm = () => {
+		console.log("submit")
+	}
+
+	const handleShowPassword = () => {
+		setShowPassword(!showPassword);
+	}
+
+	return(
+		<div className="login-form">
+			<h1>Música para todos.</h1>
+			<Form onSubmit={handleForm}>
+				<Form.Field>
+					<Input
+						type="text"
+						name="email"
+						placeholder="Correo electrónico"
+						icon="mail outline"
+					/>
+				</Form.Field>
+				<Form.Field>
+					<Input
+						type={showPassword ? "text":"password"}
+						name="password"
+						placeholder="Contraseña"
+						icon={
+							showPassword ? 
+							(<Icon name="eye slash outline" link onClick={handleShowPassword}/>) :
+							(<Icon name="eye" link onClick={handleShowPassword}/>)
+						}
+					/>
+				</Form.Field>
+				<Button type="submit">Iniciar Sesión</Button>
+			</Form>
+			<div className="login-form__options">
+				<p onClick={() => setSelectedForm(null)}>Volver</p>
+				<p>
+					¿No tienes cuenta?{" "}
+					<span onClick={() => setSelectedForm("register")}>Registrarte</span>
+				</p>
+			</div>
+		</div>
+	)
 }
 
 export default LoginForm;
